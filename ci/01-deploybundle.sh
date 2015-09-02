0cbff790666afd500ed741295c206c5bd99abf9b
Narinder Gupta <narinder.gupta@canoncial.com>
2015-09-02 08:01:56 -0500

    modified the default bundle deployment to ODL. Also updated the localtion of the openvswitch charm from local to branch.
    Change-Id: I18a8f32d5963809ea8ea1f86669f3a2cf7b458c5

--- ci/01-deploybundle.sh
+++ ci/01-deploybundle.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-#!/bin/bash
 #placeholder for deployment script.
 set -ex
 
 case "$1" in
     'nonha' )
-        cp opencontrail/juju-deployer/contrail.yaml ./bundles.yaml
+        cp odl/juju-deployer/ovs-odl.yaml ./bundles.yaml
         ;;
     'ha' )
-        cp opencontrail/juju-deployer/contrail-ha.yaml ./bundles.yaml
+        cp odl/juju-deployer/ovs-odl-ha.yaml ./bundles.yaml
         ;;
     'tip' )
-        cp opencontrail/juju-deployer/contrail-tip.yaml ./bundles.yaml
+        cp odl/juju-deployer/ovs-odl-tip.yaml ./bundles.yaml
         ;;
     * )
-        cp opencontrail/juju-deployer/contrail.yaml ./bundles.yaml
+        cp odl/juju-deployer/ovs-odl.yaml ./bundles.yaml
         ;;
 esac
 
 echo "... Deployment Started ...."
 
-echo juju-deployer -d -r 13 -c bundles.yaml trusty-"$2"-contrail
+#case openstack kilo with odl
+juju-deployer -d -r 13 -c bundles.yaml trusty-"$2"
 
 echo "... Deployment finished ...."
-
