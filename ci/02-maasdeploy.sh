d63001c410ec8502802c6f2be9edd70fb102ac75
Narinder Gupta <narinder.gupta@canonical.com>
2015-09-30 13:51:37 -0500

    Added support for Orange lab POD5 along with some renaming of the files.
    Change-Id: I30739b0a56fbe9f65dbe3fb503d53221fbb046b9

--- ci/02-maasdeploy.sh
+++ ci/02-maasdeploy.sh
@@ -4,10 +4,16 @@ set -ex
 
 case "$1" in
     'intelpod5' )
-        cp intel/pod5/maas/deployment.yaml ./deployment.yaml
+        cp maas/intel/pod5/deployment.yaml ./deployment.yaml
+        ;;
+    'intelpod6' )
+        cp maas/intel/pod6/deployment.yaml ./deployment.yaml
+        ;;
+    'orangepod2' )
+        cp maas/orange/pod2/deployment.yaml ./deployment.yaml
         ;;
     * )
-        cp intel/pod5/maas/deployment.yaml ./deployment.yaml
+        cp maas/intel/pod5/deployment.yaml ./deployment.yaml
         ;;
 esac
 
@@ -15,11 +21,29 @@ echo "... Deployment of maas Started ...."
 sudo apt-add-repository ppa:maas-deployers/stable -y
 sudo apt-get update -y
 sudo apt-get install maas-deployer -y
+
 if [ ! -e /home/ubuntu/.ssh/id_rsa ]; then
     ssh-keygen -N '' -f /home/ubuntu/.ssh/id_rsa
 fi
 sudo adduser ubuntu libvirtd
-cat /home/ubuntu/.ssh/id_rsa.pub > /home/ubuntu/.ssh/authorized_keys
+
+if [ ! 'virsh pool-list | grep default' ]; then
+
+    virsh pool-define /dev/stdin <<EOF
+<pool type='dir'>
+  <name>default</name>
+  <target>
+    <path>/var/lib/libvirt/images</path>
+  </target>
+</pool>
+EOF
+
+    virsh pool-start default
+    virsh pool-autostart default
+
+fi
+
+cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 maas-deployer -c deployment.yaml -d --force
 echo "... Deployment of maas finish ...."
 
