282c1b886b52722d38c2e789e2df960506a198d9
Narinder Gupta <narinder.gupta@canonical.com>
2015-10-26 20:23:00 -0500

    modified maas to include the ssh public keys for maas and host server
    Change-Id: I361e42d05ecc9d11b425b7d8e6a70104cb854c7c

--- ci/02-maasdeploy.sh
+++ ci/02-maasdeploy.sh
@@ -56,8 +56,11 @@ echo "... Deployment of maas finish ...."
 maas_ip=`grep " ip_address" deployment.yaml | cut -d " "  -f 10`
 apikey=`grep maas-oauth: environments.yaml | cut -d "'" -f 2`
 maas login maas http://${maas_ip}/MAAS/api/1.0 ${apikey}
-maas maas boot-source-selections create 1 os="ubuntu" release="precise" arches="amd64" subarches="*" labels="*"
+maas maas boot-source update 1 url="http://maas.ubuntu.com/images/ephemeral-v2/daily/"
+#maas maas boot-source-selections create 1 os="ubuntu" release="precise" arches="amd64" subarches="*" labels="*"
 maas maas boot-resources import
+maas maas sshkeys new key="`cat $HOME/.ssh/id_rsa.pub`"
+maas maas sshkeys new key="`cat $HOME/.ssh/id_maas.pub`"
 
 #echo "... Deployment of opnfv release Started ...."
 #python deploy.py $maas_ip
