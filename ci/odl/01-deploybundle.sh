c1b2ce98a0dd9093216e71ed97117fbd0c4d2294
Narinder Gupta <narinder.gupta@canonical.com>
2015-11-12 08:29:20 -0600

    chnage bundle as per new bundle from openstack team
    Change-Id: I0f0a47a90ad99b459594544461b220eabfea7afb
    Author: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/odl/01-deploybundle.sh
+++ ci/odl/01-deploybundle.sh
@@ -28,7 +28,7 @@ case "$3" in
         # choose the correct interface to use for data network
          sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 192.168.12.0\/24/g' ./bundles.yaml
         # Choose the external port to go out from gateway to use.
-         sed -i -- 's/#        "ext-port": "eth1"/#        "ext-port": "brPublic"/g' ./bundles.yaml
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "brPublic"/g' ./bundles.yaml
          ;;
      'intelpod6' )
          cp maas/intel/pod6/interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
@@ -40,7 +40,7 @@ case "$3" in
         # choose the correct interface to use for data network
          sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 10.4.9.0\/24/g' ./bundles.yaml
         # Choose the external port to go out from gateway to use.
-         sed -i -- 's/#        "ext-port": "eth1"/#        "ext-port": "brPublic"/g' ./bundles.yaml
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "brPublic"/g' ./bundles.yaml
          ;;
      'intelpod5' )
          cp maas/intel/pod5/interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
@@ -52,7 +52,7 @@ case "$3" in
         # choose the correct interface to use for data network
          sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 10.4.9.0\/24/g' ./bundles.yaml
         # Choose the external port to go out from gateway to use.
-         sed -i -- 's/#        "ext-port": "eth1"/#        "ext-port": "brPublic"/g' ./bundles.yaml
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth2.724"/g' ./bundles.yaml
         ;;
 esac
 
