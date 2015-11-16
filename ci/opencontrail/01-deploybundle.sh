ae62d64e874c425eb3136dc42a80d5cb41a3e7f3
Narinder Gupta <narinder.gupta@canonical.com>
2015-11-16 17:12:46 -0600

    modified the bundle and script to have contrail deployed in non ha node. modified the bundle for other release of openstack. Added the support for contrai HA and git tree.
    Change-Id: I29c5514ba8cbe2a0c6dd1ce4d2b19784b05ac282
    Author: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/opencontrail/01-deploybundle.sh
+++ ci/opencontrail/01-deploybundle.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 #placeholder for deployment script.
 set -ex
 
@@ -12,6 +11,8 @@ case "$1" in
         ;;
     'tip' )
         cp opencontrail/juju-deployer/contrail-tip.yaml ./bundles.yaml
+        cp opencontrail/juju-deployer/source/* ./
+        sed -i -- "s|branch: master|branch: stable/$2|g" ./*.yaml
         ;;
     * )
         cp opencontrail/juju-deployer/contrail.yaml ./bundles.yaml
@@ -20,31 +21,61 @@ esac
 
 case "$3" in
     'orangepod2' )
-        sed -i -- 's/10.4.1.1/192.168.2.2/g' ./bundles.yaml
-        ;;
-    'intelpod5' )
-        sed -i -- 's/10.4.1.1/10.4.1.2/g' ./bundles.yaml
-        ;;
-    'intelpod6' )
-        sed -i -- 's/10.4.1.1/10.4.1.2/g' ./bundles.yaml
+        cp maas/orange/pod2/control-interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
+        cp maas/orange/pod2/lxc-add-more-interfaces trusty/ubuntu-nodes-controller/lxc/add-more-interfaces
+        cp maas/orange/pod2/compute-interfaces.host trusty/ubuntu-nodes-compute/network/interfaces.host
+        cp maas/orange/pod2/lxc-add-more-interfaces trusty/ubuntu-nodes-compute/lxc/add-more-interfaces
+        # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
+         sed -i -- 's/10.4.1.1/192.168.2.2/g' ./bundles.yaml
+        # choose the correct interface to use for data network
+         sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 192.168.12.0\/24/g' ./bundles.yaml
+        # Choose the external port to go out from gateway to use.
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth1"/g' ./bundles.yaml
+         ;;
+     'intelpod6' )
+         cp maas/intel/pod6/interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
+         cp maas/intel/pod6/lxc-add-more-interfaces trusty/ubuntu-nodes-controller/lxc/add-more-interfaces
+         cp maas/intel/pod6/interfaces.host trusty/ubuntu-nodes-compute/network/interfaces.host
+         cp maas/intel/pod6/lxc-add-more-interfaces trusty/ubuntu-nodes-compute/lxc/add-more-interfaces
+        # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
+         sed -i -- 's/10.4.1.1/10.4.1.2/g' ./bundles.yaml
+        # choose the correct interface to use for data network
+         sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 10.4.9.0\/24/g' ./bundles.yaml
+        # Choose the external port to go out from gateway to use.
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth2"/g' ./bundles.yaml
+         ;;
+     'intelpod5' )
+         cp maas/intel/pod5/interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
+         cp maas/intel/pod5/lxc-add-more-interfaces trusty/ubuntu-nodes-controller/lxc/add-more-interfaces
+         cp maas/intel/pod5/interfaces.host trusty/ubuntu-nodes-compute/network/interfaces.host
+         cp maas/intel/pod5/lxc-add-more-interfaces trusty/ubuntu-nodes-compute/lxc/add-more-interfaces
+        # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
+         sed -i -- 's/10.4.1.1/10.4.1.2/g' ./bundles.yaml
+        # choose the correct interface to use for data network
+         sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 10.4.9.0\/24/g' ./bundles.yaml
+        # Choose the external port to go out from gateway to use.
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth2.724"/g' ./bundles.yaml
         ;;
 esac
 
 echo "... Deployment Started ...."
 case "$1" in
     'nonha' )
-        juju-deployer -vW -d -c bundles.yaml trusty-"$2"-contrail
+        juju-deployer -vW -d -c bundles.yaml trusty-"$2"-nodes
+        juju-deployer -vW -d -c bundles.yaml trusty-"$2"
         ;;
     'ha' )
-        juju-deployer -vW -d -c bundles.yaml openstack-phase1
+        juju-deployer -vW -d -c bundles.yaml trusty-"$2"-nodes
+        juju-deployer -vW -d -c bundles.yaml trusty-"$2"
         ;;
     'tip' )
-        juju-deployer -vW -d -c bundles.yaml trusty-"$2"-contrail
+        juju-deployer -vW -d -c bundles.yaml trusty-"$2"-nodes
+        juju-deployer -vW -d -c bundles.yaml trusty-"$2"
         ;;
     * )
-        juju-deployer -vW -d -c bundles.yaml trusty-"$2"-contrail
+        juju-deployer -vW -d -c bundles.yaml trusty-"$2"-nodes
+        juju-deployer -vW -d -c bundles.yaml trusty-"$2"
         ;;
 esac
 
 echo "... Deployment finished ...."
-
