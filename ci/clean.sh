074e93f87a8a872ce4b8edf2339ecbe1fd1256a2
Narinder Gupta <narinder.gupta@canonical.com>
2015-12-11 07:29:21 -0600

    modified clean and clean it properly.
    Change-Id: I61a11b1c7d76f539b8c061f5b155aa29c75a0406

--- ci/clean.sh
+++ ci/clean.sh
@@ -2,17 +2,18 @@
 
 set -ex
 
-if [ -d "~/.juju/environemnts" ]; then
-    juju status  &>status.txt || true
+if [ -d $HOME/.juju/environments ]; then
+    echo " " > status.txt
+    juju status  &>>status.txt || true
     if [ "$(grep -c "environment is not bootstrapped" status.txt )" -ge 1 ]; then
         echo " environment is not bootstrapped ..."
     else
         echo " environment is bootstrapped ..."
         juju destroy-environment demo-maas  -y
-        rm -rf ~/.juju/j*
-        rm -rf ~/.juju/.deployer-store-cache
+        rm -rf $HOME/.juju/j*
+        rm -rf $HOME/.juju/.deployer-store-cache
     fi
-    rm -rf ~/.juju/environments
-    rm -rf ~/.juju/ssh
+    rm -rf $HOME/.juju/environments
+    rm -rf $HOME/.juju/ssh
 fi
 
