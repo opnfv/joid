46ba62b3fac2b49a03a3ac0851db47ef177f73ee
Narinder Gupta <narinder.gupta@canonical.com>
2016-01-14 13:31:27 -0600

    consolidated all sdn 01-deploybundl.sh in joid/ci/01-deploybundle.sh
    Change-Id: Ic76bc50721e0c1f022d1ff877bf0faef7dd6e240

--- ci/01-deploybundle.sh
+++ ci/01-deploybundle.sh
@@ -4,27 +4,23 @@ set -ex
 
 case "$1" in
     'nonha' )
-        cp nosdn/juju-deployer/ovs.yaml ./bundles.yaml
+        cp $4/juju-deployer/ovs-$4-nonha.yaml ./bundles.yaml
         ;;
     'ha' )
-        cp nosdn/juju-deployer/ovs-ha.yaml ./bundles.yaml
+        cp $4/juju-deployer/ovs-$4-ha.yaml ./bundles.yaml
         ;;
     'tip' )
-        cp nosdn/juju-deployer/ovs-tip.yaml ./bundles.yaml
+        cp $4/juju-deployer/ovs-$4-tip.yaml ./bundles.yaml
         cp common/source/* ./
         sed -i -- "s|branch: master|branch: stable/$2|g" ./*.yaml
         ;;
     * )
-        cp nosdn/juju-deployer/ovs.yaml ./bundles.yaml
+        cp $4/juju-deployer/ovs-$4-nonha.yaml ./bundles.yaml
         ;;
 esac
 
 case "$3" in
     'orangepod2' )
-        cp maas/orange/pod2/control-interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
-        cp maas/orange/pod2/lxc-add-more-interfaces trusty/ubuntu-nodes-controller/lxc/add-more-interfaces
-        cp maas/orange/pod2/compute-interfaces.host trusty/ubuntu-nodes-compute/network/interfaces.host
-        cp maas/orange/pod2/lxc-add-more-interfaces trusty/ubuntu-nodes-compute/lxc/add-more-interfaces
         # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
          sed -i -- 's/10.4.1.1/192.168.2.2/g' ./bundles.yaml
         # choose the correct interface to use for data network
@@ -33,37 +29,29 @@ case "$3" in
          sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth1"/g' ./bundles.yaml
          ;;
      'intelpod6' )
-         cp maas/intel/pod6/interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
-         cp maas/intel/pod6/lxc-add-more-interfaces trusty/ubuntu-nodes-controller/lxc/add-more-interfaces
-         cp maas/intel/pod6/interfaces.host trusty/ubuntu-nodes-compute/network/interfaces.host
-         cp maas/intel/pod6/lxc-add-more-interfaces trusty/ubuntu-nodes-compute/lxc/add-more-interfaces
         # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
          sed -i -- 's/10.4.1.1/10.4.1.2/g' ./bundles.yaml
         # choose the correct interface to use for data network
          sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 10.4.9.0\/24/g' ./bundles.yaml
         # Choose the external port to go out from gateway to use.
-         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "brPublic"/g' ./bundles.yaml
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth2"/g' ./bundles.yaml
          ;;
      'intelpod5' )
-         cp maas/intel/pod5/interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
-         cp maas/intel/pod5/lxc-add-more-interfaces trusty/ubuntu-nodes-controller/lxc/add-more-interfaces
-         cp maas/intel/pod5/interfaces.host trusty/ubuntu-nodes-compute/network/interfaces.host
-         cp maas/intel/pod5/lxc-add-more-interfaces trusty/ubuntu-nodes-compute/lxc/add-more-interfaces
         # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
          sed -i -- 's/10.4.1.1/10.4.1.2/g' ./bundles.yaml
         # choose the correct interface to use for data network
          sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 10.4.9.0\/24/g' ./bundles.yaml
         # Choose the external port to go out from gateway to use.
-         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "brPublic"/g' ./bundles.yaml
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth2.724"/g' ./bundles.yaml
         ;;
      'attvirpod1' )
-         cp maas/att/virpod1/interfaces.host trusty/ubuntu-nodes-controller/network/interfaces.host
-         cp maas/att/virpod1/interfaces.host trusty/ubuntu-nodes-compute/network/interfaces.host
-         cp maas/att/virpod1/lxc-add-more-interfaces trusty/ubuntu-nodes-controller/lxc/add-more-interfaces
-         cp maas/att/virpod1/lxc-add-more-interfaces trusty/ubuntu-nodes-compute/lxc/add-more-interfaces
         # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
          sed -i -- 's/10.4.1.1/192.168.10.1/g' ./bundles.yaml
         # Choose the external port to go out from gateway to use.
+         sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth1"/g' ./bundles.yaml
+        ;;
+     'default' )
+         sed -i -- 's/10.4.1.1/192.168.122.1/g' ./bundles.yaml
          sed -i -- 's/#        "ext-port": "eth1"/        "ext-port": "eth1"/g' ./bundles.yaml
         ;;
 esac
@@ -72,19 +60,19 @@ echo "... Deployment Started ...."
 case "$1" in
     'nonha' )
         juju-deployer -vW -d -c bundles.yaml trusty-"$2"-nodes
-        juju-deployer -vW -d -c bundles.yaml trusty-"$2"
+        juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml trusty-"$2"
         ;;
     'ha' )
         juju-deployer -vW -d -c bundles.yaml trusty-"$2"-nodes
-        juju-deployer -vW -d -c bundles.yaml trusty-"$2"
+        juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml trusty-"$2"
         ;;
     'tip' )
         juju-deployer -vW -d -c bundles.yaml trusty-"$2"-nodes
-        juju-deployer -vW -d -c bundles.yaml trusty-"$2"
+        juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml trusty-"$2"
         ;;
     * )
         juju-deployer -vW -d -c bundles.yaml trusty-"$2"-nodes
-        juju-deployer -vW -d -c bundles.yaml trusty-"$2"
+        juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml trusty-"$2"
         ;;
 esac
 
