76af780ef57e6ff882e92d4ad6205a34a9b76202
Narinder Gupta <narinder.gupta@canonical.com>
2016-01-22 13:21:39 -0600

    adding sudo in front of VM for virtual install cleanup.
    Change-Id: I761fc980d0be39275a59592b724db427fec9d3d8

--- ci/cleanvm.sh
+++ ci/cleanvm.sh
@@ -8,20 +8,20 @@ set -ex
 echo " Cleanup Started ..."
 ./clean.sh
  
- virsh destroy opnfv-maas || true
- virsh destroy bootstrap || true
- virsh destroy node1-control || true
- virsh destroy node3-control || true
- virsh destroy node4-control || true
- virsh destroy node2-compute || true
- virsh destroy node5-compute || true
- virsh undefine opnfv-maas || true
- virsh undefine bootstrap || true
- virsh undefine node1-control || true
- virsh undefine node3-control || true
- virsh undefine node4-control || true
- virsh undefine node2-compute || true
- virsh undefine node5-compute || true
- sudo rm -rf  /var/lib/libvirt/images/opnfv-maas.img /var/lib/libvirt/images/bootstrap.img /var/lib/libvirt/images/node1-control.img /var/lib/libvirt/images/node3-control.img /var/lib/libvirt/images/node4-control.img /var/lib/libvirt/images/node2-compute.img /var/lib/libvirt/images/node5-compute.img || true
+sudo virsh destroy opnfv-maas || true
+sudo virsh destroy bootstrap || true
+sudo virsh destroy node1-control || true
+sudo virsh destroy node3-control || true
+sudo virsh destroy node4-control || true
+sudo virsh destroy node2-compute || true
+sudo virsh destroy node5-compute || true
+sudo virsh undefine opnfv-maas || true
+sudo virsh undefine bootstrap || true
+sudo virsh undefine node1-control || true
+sudo virsh undefine node3-control || true
+sudo virsh undefine node4-control || true
+sudo virsh undefine node2-compute || true
+sudo virsh undefine node5-compute || true
+sudo rm -rf  /var/lib/libvirt/images/opnfv-maas.img /var/lib/libvirt/images/bootstrap.img /var/lib/libvirt/images/node1-control.img /var/lib/libvirt/images/node3-control.img /var/lib/libvirt/images/node4-control.img /var/lib/libvirt/images/node2-compute.img /var/lib/libvirt/images/node5-compute.img || true
  
 echo " Cleanup Finished ..."
