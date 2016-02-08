069bb3a3a90422a5771052f6f2f9fd19848ebc07
Narinder Gupta <narinder.gupta@canonical.com>
2016-02-08 13:35:10 -0600

    forgot " in the last.
    Change-Id: I89d8a050c2ccf34fe98223749d644177e349d055
    (cherry picked from commit 3b421b0613b80edce3e87944a74b21f82ba4c46f)

--- ci/01-deploybundle.sh
+++ ci/01-deploybundle.sh
@@ -77,13 +77,13 @@ case "$5" in
          sed -i -- 's/#l2-population: true/l2-population: true/g' ./bundles.yaml
         ;;
     'sfc' )
-         sed -i -- 's/profile: "openvswitch-odl-Be"/profile: "openvswitch-odl-Be-sfc/g' ./bundles.yaml
+         sed -i -- 's/profile: "openvswitch-odl-Be"/profile: "openvswitch-odl-Be-sfc"/g' ./bundles.yaml
         ;;
     'vpn' )
-         sed -i -- 's/profile: "openvswitch-odl-Be"/profile: "openvswitch-odl-Be-vpn/g' ./bundles.yaml
+         sed -i -- 's/profile: "openvswitch-odl-Be"/profile: "openvswitch-odl-Be-vpn"/g' ./bundles.yaml
         ;;
     'odl_l3' )
-         sed -i -- 's/profile: "openvswitch-odl-Be"/profile: "openvswitch-odl-Be-l3/g' ./bundles.yaml
+         sed -i -- 's/profile: "openvswitch-odl-Be"/profile: "openvswitch-odl-Be-l3"/g' ./bundles.yaml
         ;;
 esac
 
