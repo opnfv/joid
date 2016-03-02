467c0afd120451f741189b12a9e1f39c4a37ca78
Narinder Gupta <narinder.gupta@canonical.com>
2016-03-02 13:56:31 -0600

    modified to include the gatway mac as part of deployment.
    Change-Id: I052dcc972d1a9381565b4576ac80aad4595c21ef
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>
    (cherry picked from commit cbc4db9cefe77a75f906e13d0d702af2b80df2ff)

--- ci/01-deploybundle.sh
+++ ci/01-deploybundle.sh
@@ -35,6 +35,8 @@ case "$3" in
          sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 10.4.9.0\/24/g' ./bundles.yaml
         # Choose the external port to go out from gateway to use.
          sed -i -- 's/#ext-port: "eth1"/ext-port: "eth2"/g' ./bundles.yaml
+        # Provide the gateway MAC to route the traffic externally.
+         sed -i -- 's/#gateway-mac: "default"/gateway-mac: "default"/g' ./bundles.yaml
          ;;
      'intelpod5' )
         # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
@@ -43,6 +45,8 @@ case "$3" in
          sed -i -- 's/#os-data-network: 10.4.8.0\/21/os-data-network: 10.4.9.0\/24/g' ./bundles.yaml
         # Choose the external port to go out from gateway to use.
          sed -i -- 's/#ext-port: "eth1"/ext-port: "eth2.724"/g' ./bundles.yaml
+        # Provide the gateway MAC to route the traffic externally.
+         sed -i -- 's/#gateway-mac: "default"/gateway-mac: "default"/g' ./bundles.yaml
         ;;
      'attvirpod1' )
         # As per your lab vip address list be deafult uses 10.4.1.11 - 10.4.1.20
