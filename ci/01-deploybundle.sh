5e5881112d62c2089dd13b805682cfe2e9b878b5
Narinder Gupta <narinder.gupta@canonical.com>
2016-07-07 23:24:02 -0500

    trying different option as delpoyment failed.
    Change-Id: I037057b59b2dd89f146cf65de03a2fe0053d19be
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/01-deploybundle.sh
+++ ci/01-deploybundle.sh
@@ -154,9 +154,8 @@ fi
 python genBundle.py  -l deployconfig.yaml  -s $var > bundles.yaml
 
 echo "... Deployment Started ...."
-    juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml $6-"$2"
-    #juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml $6-"$2"-nodes
-    #check_status
-
+    juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml $6-"$2"-nodes
     juju ssh nodes/0 "echo 512 | sudo tee /proc/sys/fs/inotify/max_user_instances"
     juju ssh nodes/1 "echo 512 | sudo tee /proc/sys/fs/inotify/max_user_instances"
+    juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml $6-"$2"
+    #check_status
