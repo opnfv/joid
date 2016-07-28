64b45fd5d932cc7b58e217956b4c41db1a341420
Narinder Gupta <narinder.gupta@canonical.com>
2016-07-28 15:08:09 -0500

    added aodh imtegration.
    Change-Id: I4a3d6c812d4610fad5ea9a79430340bcf524c931
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/nosdn/fetch-charms.sh
+++ ci/nosdn/fetch-charms.sh
@@ -5,8 +5,8 @@ mkdir -p $distro
 
 function build {
     sudo apt-get install charm-tools -y
-    (cd $distro/charm-congress; charm build -s $distro  -obuild src)
-    mv $distro/charm-congress/build/$distro/congress $distro
+    (cd $distro/charm-$1; charm build -s $distro  -obuild src)
+    mv $distro/charm-$1/build/$distro/$1 $distro
 }
 
 # openstack
@@ -15,28 +15,30 @@ bzr branch lp:~billy-olsen/charms/xenial/mongodb/trunk $distro/mongodb
 bzr branch lp:~narindergupta/opnfv/ntp $distro/ntp
 
 git clone https://github.com/gnuoy/charm-congress.git $distro/charm-congress
-build
+build congress
+git clone -b stable/16.07 https://github.com/openstack/charm-aodh.git $distro/charm-aodh
+build aodh
 
-git clone https://github.com/openstack/charm-hacluster.git $distro/hacluster
-git clone https://github.com/openstack/charm-ceilometer.git $distro/ceilometer
-git clone https://github.com/openstack/charm-ceilometer-agent.git $distro/ceilometer-agent
-git clone https://github.com/openstack/charm-ceph-mon.git $distro/ceph
-git clone https://github.com/openstack/charm-ceph-osd.git $distro/ceph-osd
-git clone https://github.com/openstack/charm-ceph-radosgw.git $distro/ceph-radosgw
-git clone https://github.com/openstack/charm-cinder.git $distro/cinder
-git clone https://github.com/openstack/charm-cinder-ceph.git $distro/cinder-ceph
-git clone https://github.com/openstack/charm-glance.git $distro/glance
-git clone https://github.com/openstack/charm-keystone.git $distro/keystone
-git clone https://github.com/openstack/charm-percona-cluster.git $distro/percona-cluster
-git clone https://github.com/openstack/charm-neutron-api.git $distro/neutron-api
-git clone https://github.com/openstack/charm-neutron-gateway.git $distro/neutron-gateway
-git clone https://github.com/openstack/charm-neutron-openvswitch.git $distro/neutron-openvswitch
-git clone https://github.com/openstack/charm-nova-cloud-controller.git $distro/nova-cloud-controller
-git clone https://github.com/openstack/charm-nova-compute.git $distro/nova-compute
-git clone https://github.com/openstack/charm-openstack-dashboard.git $distro/openstack-dashboard
-git clone https://github.com/openstack/charm-rabbitmq-server.git $distro/rabbitmq-server
-git clone https://github.com/openstack/charm-heat.git $distro/heat
-git clone https://github.com/openstack/charm-lxd.git xenial/lxd
+git clone -b stable/16.07 https://github.com/openstack/charm-hacluster.git $distro/hacluster
+git clone -b stable/16.07 https://github.com/openstack/charm-ceilometer.git $distro/ceilometer
+git clone -b stable/16.07 https://github.com/openstack/charm-ceilometer-agent.git $distro/ceilometer-agent
+git clone -b stable/16.07 https://github.com/openstack/charm-ceph-mon.git $distro/ceph
+git clone -b stable/16.07 https://github.com/openstack/charm-ceph-osd.git $distro/ceph-osd
+git clone -b stable/16.07 https://github.com/openstack/charm-ceph-radosgw.git $distro/ceph-radosgw
+git clone -b stable/16.07 https://github.com/openstack/charm-cinder.git $distro/cinder
+git clone -b stable/16.07 https://github.com/openstack/charm-cinder-ceph.git $distro/cinder-ceph
+git clone -b stable/16.07 https://github.com/openstack/charm-glance.git $distro/glance
+git clone -b stable/16.07 https://github.com/openstack/charm-keystone.git $distro/keystone
+git clone -b stable/16.07 https://github.com/openstack/charm-percona-cluster.git $distro/percona-cluster
+git clone -b stable/16.07 https://github.com/openstack/charm-neutron-api.git $distro/neutron-api
+git clone -b stable/16.07 https://github.com/openstack/charm-neutron-gateway.git $distro/neutron-gateway
+git clone -b stable/16.07 https://github.com/openstack/charm-neutron-openvswitch.git $distro/neutron-openvswitch
+git clone -b stable/16.07 https://github.com/openstack/charm-nova-cloud-controller.git $distro/nova-cloud-controller
+git clone -b stable/16.07 https://github.com/openstack/charm-nova-compute.git $distro/nova-compute
+git clone -b stable/16.07 https://github.com/openstack/charm-openstack-dashboard.git $distro/openstack-dashboard
+git clone -b stable/16.07 https://github.com/openstack/charm-rabbitmq-server.git $distro/rabbitmq-server
+git clone -b stable/16.07 https://github.com/openstack/charm-heat.git $distro/heat
+git clone -b stable/16.07 https://github.com/openstack/charm-lxd.git xenial/lxd
 
 #charm pull cs:~openstack-charmers-next/hacluster $distro/hacluster
 #charm pull cs:~openstack-charmers-next/ceilometer $distro/ceilometer
