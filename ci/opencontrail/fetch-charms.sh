7070d937e339f1e6b4c6e9daca39f45a6207ff2b
Narinder Gupta <narinder.gupta@canonical.com>
2017-02-06 09:23:12 -0600

    modified to use /srv instead of /dev/sdb for ceph.
    Change-Id: I9eb2e8ecdd5cb6e1c97ff03462208cea95eb2c8a
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/opencontrail/fetch-charms.sh
+++ ci/opencontrail/fetch-charms.sh
@@ -20,26 +20,26 @@ charm pull cs:$distro/aodh $distro/aodh
 git clone https://github.com/gnuoy/charm-congress.git $distro/charm-congress
 build congress
 
-git clone -b stable/16.10 https://github.com/openstack/charm-hacluster.git $distro/hacluster
+git clone https://github.com/openstack/charm-hacluster.git $distro/hacluster
 #bzr branch lp:~sdn-charmers/charms/$distro/ceilometer/ceilometer-plugin $distro/ceilometer
-#git clone -b stable/16.10 https://github.com/openstack/charm-ceilometer.git $distro/ceilometer
-#git clone -b stable/16.10 https://github.com/openstack/charm-ceilometer-agent.git $distro/ceilometer-agent
-git clone -b stable/16.10 https://github.com/openstack/charm-ceph-mon.git $distro/ceph-mon
-git clone -b stable/16.10 https://github.com/openstack/charm-ceph-osd.git $distro/ceph-osd
-git clone -b stable/16.10 https://github.com/openstack/charm-ceph-radosgw.git $distro/ceph-radosgw
-git clone -b stable/16.10 https://github.com/openstack/charm-cinder.git $distro/cinder
-git clone -b stable/16.10 https://github.com/openstack/charm-cinder-ceph.git $distro/cinder-ceph
-git clone -b stable/16.10 https://github.com/openstack/charm-glance.git $distro/glance
-git clone -b stable/16.10 https://github.com/openstack/charm-keystone.git $distro/keystone
-git clone -b stable/16.10 https://github.com/openstack/charm-percona-cluster.git $distro/percona-cluster
-git clone -b stable/16.10 https://github.com/openstack/charm-neutron-gateway.git $distro/neutron-gateway
-#git clone -b stable/16.10 https://github.com/openstack/charm-neutron-openvswitch.git $distro/neutron-openvswitch
-git clone -b stable/16.10 https://github.com/openstack/charm-nova-cloud-controller.git $distro/nova-cloud-controller
-git clone -b stable/16.10 https://github.com/openstack/charm-nova-compute.git $distro/nova-compute
-git clone -b stable/16.10 https://github.com/openstack/charm-openstack-dashboard.git $distro/openstack-dashboard
-git clone -b stable/16.10 https://github.com/openstack/charm-rabbitmq-server.git $distro/rabbitmq-server
-git clone -b stable/16.10 https://github.com/openstack/charm-heat.git $distro/heat
-git clone -b stable/16.10 https://github.com/openstack/charm-neutron-api.git $distro/neutron-api
+#git clone https://github.com/openstack/charm-ceilometer.git $distro/ceilometer
+#git clone https://github.com/openstack/charm-ceilometer-agent.git $distro/ceilometer-agent
+git clone https://github.com/openstack/charm-ceph-mon.git $distro/ceph-mon
+git clone https://github.com/openstack/charm-ceph-osd.git $distro/ceph-osd
+git clone https://github.com/openstack/charm-ceph-radosgw.git $distro/ceph-radosgw
+git clone https://github.com/openstack/charm-cinder.git $distro/cinder
+git clone https://github.com/openstack/charm-cinder-ceph.git $distro/cinder-ceph
+git clone https://github.com/openstack/charm-glance.git $distro/glance
+git clone https://github.com/openstack/charm-keystone.git $distro/keystone
+git clone https://github.com/openstack/charm-percona-cluster.git $distro/percona-cluster
+git clone https://github.com/openstack/charm-neutron-gateway.git $distro/neutron-gateway
+#git clone https://github.com/openstack/charm-neutron-openvswitch.git $distro/neutron-openvswitch
+git clone https://github.com/openstack/charm-nova-cloud-controller.git $distro/nova-cloud-controller
+git clone https://github.com/openstack/charm-nova-compute.git $distro/nova-compute
+git clone https://github.com/openstack/charm-openstack-dashboard.git $distro/openstack-dashboard
+git clone https://github.com/openstack/charm-rabbitmq-server.git $distro/rabbitmq-server
+git clone https://github.com/openstack/charm-heat.git $distro/heat
+git clone https://github.com/openstack/charm-neutron-api.git $distro/neutron-api
 
 #charm pull cs:~openstack-charmers-next/hacluster $distro/hacluster
 #charm pull cs:~openstack-charmers-next/ceilometer $distro/ceilometer
