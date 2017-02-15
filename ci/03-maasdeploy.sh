5fb8d23f322a0874e5a61c834108c84536f0466c
Narinder Gupta <narinder.gupta@canonical.com>
2017-02-15 16:21:09 -0600

    modified to enable the interface with internal-api space for ddeployment on bare metal.
    Change-Id: Id382768c7dc6a475d6bf330b9ffed590fb780ec4
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/03-maasdeploy.sh
+++ ci/03-maasdeploy.sh
@@ -269,30 +269,11 @@ enablesubnetanddhcp(){
     maas $PROFILE subnet update $SUBNET_CIDR gateway_ip=$MY_GATEWAY || true
     maas $PROFILE subnet update $SUBNET_CIDR dns_servers=$MY_NAMESERVER || true
 
-}
+    #below command will enable the interface with internal-api space.
 
-## derived from https://gist.github.com/epiloque/8cf512c6d64641bde388
-## works for arrays of hashes, as long as the hashes do not have arrays
-parse_yaml2() {
-    local prefix=$2
-    local s
-    local w
-    local fs
-    s='[[:space:]]*'
-    w='[a-zA-Z0-9_]*'
-    fs="$(echo @|tr @ '\034')"
-    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
-    awk -F"$fs" '{
-      indent = length($1)/2;
-      if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-              vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-              printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
-      }
-    }' | sed 's/_=/+=/g'
+    SPACEID=$(maas $PROFILE space read internal-api | jq '.id')
+    maas $PROFILE subnet update $SUBNET_CIDR space=$SPACEID || true
+
 }
 
 addnodes(){
