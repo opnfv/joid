f7a079a2d67b4cdc50896b63362e20aaa1c8d591
Narinder Gupta <narinder.gupta@canonical.com>
2017-03-10 09:24:34 -0600

    adding neutron-l3-agent
    Change-Id: Idf3c25c61a3b182b67b1245b53933fbf82723887
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/odl/fetch-charms.sh
+++ ci/odl/fetch-charms.sh
@@ -71,5 +71,5 @@ charm pull cs:~cloudscaling/scaleio-gui $distro/scaleio-gui
 charm pull cs:$distro/odl-controller $distro/odl-controller
 charm pull cs:~narindergupta/neutron-api-odl-11 $distro/neutron-api-odl
 charm pull cs:~narindergupta/openvswitch-odl-3 $distro/openvswitch-odl
-charm pull cs:~narindergupta/neutron-gateway-5 $distro/neutron-gateway
+charm pull cs:~narindergupta/neutron-gateway-6 $distro/neutron-gateway
 charm pull cs:~narindergupta/congress-0 $distro/congress
