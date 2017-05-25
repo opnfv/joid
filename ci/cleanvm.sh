180d78b5a7da5f2b936ab4a63cf1dad5cd7f78c6
narindergupta <narinder.gupta@canonical.com>
2017-05-25 09:12:34 -0700

    clean the vm which is defined as per the labconfig.yaml
    Change-Id: If659da0a454f05f3ed7c1e212fd7e61230741343
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/cleanvm.sh
+++ ci/cleanvm.sh
@@ -11,7 +11,8 @@ echo " Cleanup Started ..."
 vm_list=$(sudo virsh list --all)
 
 for vm in node1-control node2-compute node3-control node4-control \
-          node5-compute bootstrap
+          node5-compute rack-vir-m1 rack-vir-m2 rack-vir-m3 rack-vir-m4 \
+          rack-vir-m1 bootstrap
 do
     echo "$vm_list" | grep -q " $vm " || continue
     sudo virsh destroy $vm
