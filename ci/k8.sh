6dd155877ed597c8b5d881c80fa50f9bd810a0e1
Guo Ruijing <ruijing.guo@intel.com>
2017-05-31 10:33:17 -0700

    move k8s command from jumphost to kubernetes-master
    Change-Id: I5e1a83cebf6f90beebc34798c8b2c9a96cce4afb
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- ci/k8.sh
+++ ci/k8.sh
@@ -2,24 +2,10 @@
 set -ex
 juju run-action kubernetes-worker/0 microbot replicas=3
 sleep 30
-mkdir -p ~/.kube || true
-juju scp kubernetes-master/0:config ~/.kube/config || true
-
-if [ ! -f /snap/kubectl/current/kubectl ]; then
-  sudo apt-get install snapd -y
-  sudo snap install kubectl --classic
-fi
-
-if [ ! -f ./kubectl ]; then
-  ln -s /snap/kubectl/current/kubectl ./kubectl
-fi
-
-./kubectl cluster-info || true
 juju config kubernetes-master enable-dashboard-addons=true || true
-#./kubectl proxy
-#http://localhost:8001/ui
-./kubectl get nodes || true
-#./kubectl create -f example.yaml || true
-./kubectl get pods --all-namespaces || true
-./kubectl get services,endpoints,ingress --all-namespaces || true
 juju expose kubernetes-worker || true
+
+juju ssh kubernetes-master/0 "/snap/bin/kubectl cluster-info"
+juju ssh kubernetes-master/0 "/snap/bin/kubectl get nodes"
+juju ssh kubernetes-master/0 "/snap/bin/kubectl get pods --all-namespaces"
+juju ssh kubernetes-master/0 "/snap/bin/kubectl get services,endpoints,ingress --all-namespaces"
