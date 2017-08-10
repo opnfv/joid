19c03fed983fbb7dcb4fbed3ddfbf055a487990b
AakashKT <aakash.kt@research.iiit.ac.in>
2017-08-10 23:41:35 +0530

    Switched to correct installation method for docker in clearwater deployment scripts
    Change-Id: Id6551c43488d5fbdef169285c5741e22bd00bea8
    Signed-off-by: Aakash KT <aakash.kt@research.iiit.ac.in>

--- juju/k8s/clearwater/deploy_clearwater_k8s.sh
+++ juju/k8s/clearwater/deploy_clearwater_k8s.sh
@@ -18,7 +18,13 @@
 
 function setup_docker() {
 	echo "Installing docker..."
-	sudo apt-get install -y docker.io
+	sudo apt-get update
+	sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository \
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get install docker-ce
 
 	sudo docker login --username="aakashkt" --password="joid123"
 }
