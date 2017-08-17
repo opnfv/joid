3c848787b3c8f2bdbf7723a5178b688e018f1e95
Narinder Gupta <narinder.gupta@canonical.com>
2017-08-17 07:28:10 -0500

    cleanup and remove unnecessary code.
    Change-Id: Idace8f5b31479dbcd8040fc41ee20e3a70b0cc8a
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/02-deploybundle.sh
+++ ci/02-deploybundle.sh
@@ -151,25 +151,13 @@ fi
 #keep the back in cloud for later debugging.
 pastebinit bundles.yaml || true
 
-if [[ "$jujuver" < "2" ]]; then
-    echo "... Deployment Started ...."
-    juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml $opnfvdistro-"$openstack"-nodes
-    count=`juju status nodes --format=short | grep nodes | wc -l`
-    c=0
-    while [ $c -lt $count ]; do
-        juju ssh nodes/$c 'echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p' || true
-        juju ssh nodes-compute/$c 'echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p' || true
-        juju ssh nodes/$c 'echo 2048 | sudo tee /proc/sys/fs/inotify/max_user_instances' || true
-        juju ssh nodes-compute/$c 'echo 2048 | sudo tee /proc/sys/fs/inotify/max_user_instances' || true
-        let c+=1
-    done
+# with JUJU 2.0 bundles has to be deployed only once.
+juju deploy bundles.yaml --debug
+sleep 120
+check_status allocating
 
-    juju-deployer -vW -d -t 7200 -r 5 -c bundles.yaml $opnfvdistro-"$openstack" || true
-else
-    # with JUJU 2.0 bundles has to be deployed only once.
-    juju deploy bundles.yaml --debug
-    sleep 120
-    check_status allocating
+# need to revisit later if not needed we will remove the below.
+openfile_fix() {
     # seeing issue related to number of open files.
     count=`juju status nodes --format=short | grep nodes | wc -l`
     c=0
@@ -180,7 +168,7 @@ else
         juju ssh nodes-compute/$c 'echo 2048 | sudo tee /proc/sys/fs/inotify/max_user_instances' || true
         let c+=1
     done
-fi
+}
 
 if [ "$opnfvsdn" = "ocl" ]
 then
