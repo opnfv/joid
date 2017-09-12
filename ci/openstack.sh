107189fd319369756f5aff492fcbe39b287dbf57
Narinder Gupta <narinder.gupta@canonical.com>
2017-09-12 03:28:21 -0500

    syntax error
    Change-Id: Ibfb06a70c41df07a5517367eb96b4d6e28f8a659
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/openstack.sh
+++ ci/openstack.sh
@@ -169,8 +169,8 @@ if [ "onos" == "$opnfvsdn" ]; then
     launch_eth
     neutron net-show ext-net > /dev/null 2>&1 || neutron net-create ext-net \
                                                    --router:external=True
-if [ "ocl" == "$opnfvsdn" ]; then
-    neutron net-show ext-net > /dev/null 2>&1 || neutron net-create ext-net 
+elif [ "ocl" == "$opnfvsdn" ]; then
+    neutron net-show ext-net > /dev/null 2>&1 || neutron net-create ext-net
 
 else
     neutron net-show ext-net > /dev/null 2>&1 || neutron net-create ext-net \
@@ -186,9 +186,9 @@ neutron subnet-show ext-subnet > /dev/null 2>&1 || neutron subnet-create ext-net
 # Ocl can push packets to the fabric network in order to reach a gateway if BGP/L3VPN hasn't been configured.
 if [ "ocl" == "$opnfvsdn" ]; then
     echo "Creating simple gateway functions on ocl vRouters"
-      juju run --application nova-compute "sudo docker exec contrail-controller \
-        python /opt/contrail/utils/provision_vgw_interface.py\
-         --oper create --interface vgw1 --subnets $EXTNET_NET --routes 0.0.0.0/24 --vrf default-domain:admin:ext-net:ext-net" 
+      juju run --application nova-compute " sudo docker exec contrail-controller \
+        python /opt/contrail/utils/provision_vgw_interface.py \
+         --oper create --interface vgw1 --subnets $EXTNET_NET --routes 0.0.0.0/24 --vrf default-domain:admin:ext-net:ext-net "
 fi
 
 #congress team is not updating and supporting charm anymore so defer it.
