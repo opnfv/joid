85be80d968e676c4f0eeb85ecdc54867a43bafe7
Narinder Gupta <narinder.gupta@canonical.com>
2017-09-12 17:04:28 -0500

    AS stable/17.08 release so switching to 17.08 stable now.
    Change-Id: I7d27dfb372814591970e29425c1de2080d19b641
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/onos/fetch-charms.sh
+++ ci/onos/fetch-charms.sh
@@ -14,24 +14,24 @@ bzr branch lp:~narindergupta/charms/trusty/promise/trunk $distro/promise
 bzr branch lp:~billy-olsen/charms/xenial/mongodb/trunk $distro/mongodb
 bzr branch lp:~narindergupta/opnfv/ntp $distro/ntp
 
-git clone -b stable/17.02 https://github.com/openstack/charm-hacluster.git $distro/hacluster
-git clone -b stable/17.02 https://github.com/openstack/charm-ceilometer.git $distro/ceilometer
-git clone -b stable/17.02 https://github.com/openstack/charm-ceilometer-agent.git $distro/ceilometer-agent
-git clone -b stable/17.02 https://github.com/openstack/charm-ceph-mon.git $distro/ceph-mon
-git clone -b stable/17.02 https://github.com/openstack/charm-ceph-osd.git $distro/ceph-osd
-git clone -b stable/17.02 https://github.com/openstack/charm-ceph-radosgw.git $distro/ceph-radosgw
-git clone -b stable/17.02 https://github.com/openstack/charm-cinder.git $distro/cinder
-git clone -b stable/17.02 https://github.com/openstack/charm-cinder-ceph.git $distro/cinder-ceph
-git clone -b stable/17.02 https://github.com/openstack/charm-glance.git $distro/glance
-git clone -b stable/17.02 https://github.com/openstack/charm-keystone.git $distro/keystone
-git clone -b stable/17.02 https://github.com/openstack/charm-percona-cluster.git $distro/percona-cluster
-git clone -b stable/17.02 https://github.com/openstack/charm-neutron-api.git $distro/neutron-api
-git clone -b stable/17.02 https://github.com/openstack/charm-neutron-openvswitch.git $distro/neutron-openvswitch
-git clone -b stable/17.02 https://github.com/openstack/charm-nova-cloud-controller.git $distro/nova-cloud-controller
-git clone -b stable/17.02 https://github.com/openstack/charm-nova-compute.git $distro/nova-compute
-git clone -b stable/17.02 https://github.com/openstack/charm-openstack-dashboard.git $distro/openstack-dashboard
-git clone -b stable/17.02 https://github.com/openstack/charm-rabbitmq-server.git $distro/rabbitmq-server
-git clone -b stable/17.02 https://github.com/openstack/charm-heat.git $distro/heat
+git clone -b stable/17.08 https://github.com/openstack/charm-hacluster.git $distro/hacluster
+git clone -b stable/17.08 https://github.com/openstack/charm-ceilometer.git $distro/ceilometer
+git clone -b stable/17.08 https://github.com/openstack/charm-ceilometer-agent.git $distro/ceilometer-agent
+git clone -b stable/17.08 https://github.com/openstack/charm-ceph-mon.git $distro/ceph-mon
+git clone -b stable/17.08 https://github.com/openstack/charm-ceph-osd.git $distro/ceph-osd
+git clone -b stable/17.08 https://github.com/openstack/charm-ceph-radosgw.git $distro/ceph-radosgw
+git clone -b stable/17.08 https://github.com/openstack/charm-cinder.git $distro/cinder
+git clone -b stable/17.08 https://github.com/openstack/charm-cinder-ceph.git $distro/cinder-ceph
+git clone -b stable/17.08 https://github.com/openstack/charm-glance.git $distro/glance
+git clone -b stable/17.08 https://github.com/openstack/charm-keystone.git $distro/keystone
+git clone -b stable/17.08 https://github.com/openstack/charm-percona-cluster.git $distro/percona-cluster
+git clone -b stable/17.08 https://github.com/openstack/charm-neutron-api.git $distro/neutron-api
+git clone -b stable/17.08 https://github.com/openstack/charm-neutron-openvswitch.git $distro/neutron-openvswitch
+git clone -b stable/17.08 https://github.com/openstack/charm-nova-cloud-controller.git $distro/nova-cloud-controller
+git clone -b stable/17.08 https://github.com/openstack/charm-nova-compute.git $distro/nova-compute
+git clone -b stable/17.08 https://github.com/openstack/charm-openstack-dashboard.git $distro/openstack-dashboard
+git clone -b stable/17.08 https://github.com/openstack/charm-rabbitmq-server.git $distro/rabbitmq-server
+git clone -b stable/17.08 https://github.com/openstack/charm-heat.git $distro/heat
 git clone https://github.com/openstack/charm-lxd.git $distro/lxd
 git clone https://github.com/openbaton/juju-charm.git $distro/openbaton
 
@@ -46,26 +46,6 @@ charm pull cs:~cloudscaling/scaleio-sdc $distro/scaleio-sdc
 charm pull cs:~cloudscaling/scaleio-openstack $distro/scaleio-openstack
 charm pull cs:~cloudscaling/scaleio-cluster $distro/scaleio-cluster
 charm pull cs:~cloudscaling/scaleio-gui $distro/scaleio-gui
-
-#charm pull cs:~openstack-charmers-next/hacluster $distro/hacluster
-#charm pull cs:~openstack-charmers-next/ceilometer $distro/ceilometer
-#charm pull cs:~openstack-charmers-next/ceilometer-agent $distro/ceilometer-agent
-#charm pull cs:~openstack-charmers-next/ceph-mon $distro/ceph
-#charm pull cs:~openstack-charmers-next/ceph-osd $distro/ceph-osd
-#charm pull cs:~openstack-charmers-next/ceph-radosgw $distro/ceph-radosgw
-#charm pull cs:~openstack-charmers-next/cinder $distro/cinder
-#charm pull cs:~openstack-charmers-next/cinder-ceph $distro/cinder-ceph
-#charm pull cs:~openstack-charmers-next/glance $distro/glance
-#charm pull cs:~openstack-charmers-next/keystone $distro/keystone
-#charm pull cs:~openstack-charmers-next/percona-cluster $distro/percona-cluster
-#charm pull cs:~openstack-charmers-next/neutron-api $distro/neutron-api
-#charm pull cs:~openstack-charmers-next/neutron-openvswitch $distro/neutron-openvswitch
-#charm pull cs:~openstack-charmers-next/nova-cloud-controller $distro/nova-cloud-controller
-#charm pull cs:~openstack-charmers-next/nova-compute $distro/nova-compute
-#charm pull cs:~openstack-charmers-next/openstack-dashboard $distro/openstack-dashboard
-#charm pull cs:~openstack-charmers-next/rabbitmq-server $distro/rabbitmq-server
-#charm pull cs:~openstack-charmers-next/heat $distro/heat
-#charm pull cs:~openstack-charmers-next/lxd xenial/lxd
 
 # Controller specific charm
 bzr branch lp:~zhangyuanyou/onosfw/onos-controller $distro/onos-controller
