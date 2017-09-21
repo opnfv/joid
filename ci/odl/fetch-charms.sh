406b485a394ad2f1231a7c37ae781d7e005cb5d9
Narinder Gupta <narinder.gupta@canonical.com>
2017-09-21 09:20:34 +0000

    Revert "pull in heat bug fix."
    This reverts commit 533dfeac9078ec05bc1c755078b4da74e8c5266d.
    
    Change-Id: Icf04741d6ee463155d1d58274884deca8a625975
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/odl/fetch-charms.sh
+++ ci/odl/fetch-charms.sh
@@ -31,9 +31,6 @@ git clone -b stable/17.08 https://github.com/openstack/charm-nova-compute.git $d
 git clone -b stable/17.08 https://github.com/openstack/charm-openstack-dashboard.git $distro/openstack-dashboard
 git clone -b stable/17.08 https://github.com/openstack/charm-rabbitmq-server.git $distro/rabbitmq-server
 git clone -b stable/17.08 https://github.com/openstack/charm-heat.git $distro/heat
-cd $distro/heat
-git pull https://git.openstack.org/openstack/charm-heat refs/changes/19/504219/1
-cd ../../
 git clone -b stable/17.08 https://github.com/openstack/charm-lxd.git $distro/lxd
 git clone -b stable/17.08 https://github.com/openstack/charm-odl-controller.git $distro/odl-controller
 git clone -b stable/17.08 https://github.com/openstack/charm-neutron-api-odl.git $distro/neutron-api-odl
