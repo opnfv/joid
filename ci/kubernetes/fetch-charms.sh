f3f3a1fee413ed5e302193c4e4533ef1dbc17b4e
Narinder Gupta <narinder.gupta@canonical.com>
2017-12-01 11:12:17 -0600

    added new ntp charm to remove the K8 issues.
    Change-Id: If50988dfda960bc7f592289f947b42a6d3fa3dc1
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/kubernetes/fetch-charms.sh
+++ ci/kubernetes/fetch-charms.sh
@@ -10,7 +10,7 @@ function build {
 }
 
 # openstack
-bzr branch lp:~narindergupta/opnfv/ntp $distro/ntp
+charm pull cs:ntp $distro/ntp
 git clone -b stable/17.08 https://github.com/openstack/charm-ceph-mon.git $distro/ceph-mon
 git clone -b stable/17.08 https://github.com/openstack/charm-ceph-osd.git $distro/ceph-osd
 
--- ci/onos/fetch-charms.sh
+++ ci/onos/fetch-charms.sh
@@ -12,7 +12,6 @@ function build {
 # openstack
 bzr branch lp:~narindergupta/charms/trusty/promise/trunk $distro/promise
 bzr branch lp:~billy-olsen/charms/xenial/mongodb/trunk $distro/mongodb
-bzr branch lp:~narindergupta/opnfv/ntp $distro/ntp
 
 git clone -b stable/17.08 https://github.com/openstack/charm-hacluster.git $distro/hacluster
 git clone -b stable/17.08 https://github.com/openstack/charm-ceilometer.git $distro/ceilometer
@@ -35,6 +34,7 @@ git clone -b stable/17.08 https://github.com/openstack/charm-heat.git $distro/he
 git clone https://github.com/openstack/charm-lxd.git $distro/lxd
 git clone https://github.com/openbaton/juju-charm.git $distro/openbaton
 
+charm pull cs:ntp $distro/ntp
 charm pull cs:$distro/aodh $distro/aodh
 charm pull cs:~narindergupta/congress-1 $distro/congress
 
