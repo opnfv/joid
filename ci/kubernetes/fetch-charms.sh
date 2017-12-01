9d408adef86bd1c291ab8fbcbfac659ffa0b20ef
Narinder Gupta <narinder.gupta@canonical.com>
2017-12-01 11:49:12 -0600

    for kubernetes bug fix
    Change-Id: Ib13fb89c30d016d0bf77a1bab62d2fbdafeb57af
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>

--- ci/kubernetes/fetch-charms.sh
+++ ci/kubernetes/fetch-charms.sh
@@ -11,6 +11,3 @@ function build {
 
 # openstack
 bzr branch lp:~narindergupta/opnfv/ntp $distro/ntp
-git clone -b stable/17.08 https://github.com/openstack/charm-ceph-mon.git $distro/ceph-mon
-git clone -b stable/17.08 https://github.com/openstack/charm-ceph-osd.git $distro/ceph-osd
-
