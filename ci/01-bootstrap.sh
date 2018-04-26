a9ea89a4bc4258e1aceb0f7aef4b2af5506b212b
Narinder Gupta <narinder.gupta@canonical.com>
2018-04-26 07:50:38 -0500

    making default series to xenial.
    Change-Id: I26dfb8941e7b00717121dac3f1b3ecb26d8210bb
    Signed-off-by: Narinder Gupta <narinder.gupta@canonical.com>
    (cherry picked from commit 5235de0b3205b6d7e9c63cc85600927226a755d6)

--- ci/01-bootstrap.sh
+++ ci/01-bootstrap.sh
@@ -4,4 +4,4 @@ set -ex
 
 controllername=`awk 'NR==1{print substr($1, 1, length($1)-1)}' deployconfig.yaml`
 cloudname=`awk 'NR==1{print substr($1, 1, length($1)-1)}' deployconfig.yaml`
-juju bootstrap $controllername $cloudname --debug --to bootstrap.maas
+juju bootstrap $controllername $cloudname --debug --to bootstrap.maas --bootstrap-series=xenial
